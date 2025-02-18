f68b03fbe5abe974a50794fbe913b57ccb9e1e79
mohdahmad2013 <ziena.saif@gmail.com>
2025-02-18 08:30:54 -0800

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,7 @@
+def weatherandclimate(w,c):
+    print("The weather is ",w)
+    print("The climate is ",c)
+w=input("Enter the weather ")
+c=input("Enter the climate ")
+weatherandclimate(w,c)
+    
